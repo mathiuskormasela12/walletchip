a57aea8f80ee6ece11583b4ed2bef2c84ea792c2
zaqijr7 <zaqijr7@gmail.com>
2021-02-14 20:22:37 +0700

    add flow transaction feature

--- src/views/Dashboard/Home/index.js
+++ src/views/Dashboard/Home/index.js
@@ -1,19 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
 //import example photo profile
-import photoProfile from '../../../assets/images/1.png'
 import { Footer, Navbar } from '../../../components'
 
 // import css
 import './home.css'
+import http from '../../../helpers/Http'
 
 function Home() {
-  const user = useSelector(state => state.user)
-  
+  const [listHistory, setListHistory] = useState(null)
+  const [msgResponse, setMsgResponse] = useState('')
+  const [isLoading, setIsLoading] = useState(true)
+  // const [isLoading, isLoading] = useState(false)
+  const user = useSelector(state => state.user.userDetail)
+  const [balance, setBalance] = useState('')
+  const token = useSelector(state => state.auth.token)
+
+  const getTransactionHistory = async (token) => {
+    try {
+      const response = await http(token).get('transaction-history')
+      if (response.data.results) {
+        setIsLoading(!isLoading)
+        setListHistory(response.data.results)
+      } else {
+        setIsLoading(!isLoading)
+        setListHistory(null)
+        setMsgResponse(response.data.message)
+      }
+    } catch (err) {
+      setMsgResponse(err.response.data.message);
+    }
+  }
+
+  const getBalance = async (token) => {
+    try {
+      const user = await http(token).get(`dashboard/profile`)
+      setBalance(user.data.results.balance)
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    getTransactionHistory(token)
+    getBalance(token)
+    // setTimeout(() => {
+    //   setIsLoading(!isLoading)
+    // }, 1500)
+  }, [])
+  console.log(msgResponse);
   React.useEffect(() => {
-    document.title = `${user.firstName}'s Dashboard - Walletchip`
+    document.title = `${user.first_name}'s Dashboard - Walletchip`
   }, [])
 
   return (
@@ -28,27 +67,27 @@ function Home() {
                     <p style={{ color: "#E0E0E0" }}>Balance</p>
                   </div>
                   <div className="row">
-                    <h2 className="fw-bold text-white">Rp.{`120.000`}</h2>
+                    <h2 className="fw-bold text-white">Rp.{`${balance}`}</h2>
                   </div>
                   <div className="row mt-2">
-                    <small style={{ color: "#E0E0E0" }}>+62 813-9387-7946</small>
+                    <small style={{ color: "#E0E0E0" }}>{`${user.phone === null ? 'Update Your Phone' : user.phone}`}</small>
                   </div>
                 </div>
                 <div className="col-3">
                   <div className="row my-3">
                     <div className="d-grid">
-                      <button className="btn border border-2 py-2 btn-balance-control fw-bold text-white">
+                      <button className="btn border border-2 py-2 btn-balance-control fw-bold">
                         <i className="far fa-arrow-up me-3" style={{ color: "#E0E0E0" }}></i>
-                    Transfer
-                  </button>
+                        <Link to="/transfer" className="text-decoration-none text-white"> Transfer </Link>
+                      </button>
                     </div>
                   </div>
                   <div className="row my-3">
                     <div className="d-grid">
                       <button className="btn border border-2 py-2 btn-balance-control fw-bold text-white">
                         <i className="far fa-plus me-3" style={{ color: "#E0E0E0" }}></i>
-                    Topup
-                  </button>
+                        Topup
+                      </button>
                     </div>
                   </div>
                 </div>
@@ -71,41 +110,69 @@ function Home() {
               </div>
             </div>
             <div className="card-body overflow-auto card-transaction-history">
-              {
-                [1, 2, 3, 4].map(transaction => {
-                  return (
-                    <>
-                      <div className="row my-1" key={String(transaction)}>
-                        <div className="card">
-                          <div className="card-body">
-                            <div className="row">
-                              <div className="col-1 d-flex justify-content-center align-items-center">
-                                <div className="row">
-                                  <img src={photoProfile} alt="avatar" className="img-profile-avatar" />
-                                </div>
-                              </div>
-                              <div className="col-9">
-                                <div className="row">
-                                  <span className="fw-bold">Samuel Suhi</span>
-                                  <small className="text-muted">
-                                    Transfer
-                              </small>
-                                </div>
-                              </div>
-                              <div className="col-2 d-flex align-items-center justify-content-center">
-                                <div>
-                                  <span className="fw-bold transfer-color">
-                                    +Rp.{`50.000`}
-                                  </span>
+              {listHistory === null ?
+                <>
+                  {
+                    isLoading === true ?
+                      <>
+                        <div className="d-flex justify-content-center align-items-center width-100">
+                          <div className="spinner-border" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                          </div>
+                        </div>
+                      </>
+                      :
+                      'User has no transactional history'
+                  }
+                </>
+                :
+                <>
+                  {
+                    isLoading === true ?
+                      <>
+                        <div className="d-flex justify-content-center align-items-center width-100">
+                          <div className="spinner-border" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                          </div>
+                        </div>
+                      </>
+                      :
+                      listHistory.map((transaction, index) => {
+                        return (
+                          <>
+                            <div className="row my-1" key={String(index)}>
+                              <div className="card">
+                                <div className="card-body">
+                                  <div className="row">
+                                    <div className="col-1 d-flex justify-content-center align-items-center">
+                                      <div className="row">
+                                        <img src={transaction.picture} alt="avatar" className="img-profile-avatar" />
+                                      </div>
+                                    </div>
+                                    <div className="col-9">
+                                      <div className="row">
+                                        <span className="fw-bold">{transaction.another_user}</span>
+                                        <small className="text-muted">
+                                          {transaction.did_user_transfer === 1 ? 'Subscription' : 'Transfer'}
+                                        </small>
+                                      </div>
+                                    </div>
+                                    <div className="col-2 d-flex align-items-center justify-content-center">
+                                      <div>
+                                        <span className={`fw-bold ${transaction.did_user_transfer === 1 ? 'subsription-color' : 'transfer-color'}`}>
+                                          {transaction.did_user_transfer === 1 ? `-${transaction.amount}` : `+${transaction.amount}`}
+                                        </span>
+                                      </div>
+                                    </div>
+                                  </div>
                                 </div>
                               </div>
                             </div>
-                          </div>
-                        </div>
-                      </div>
-                    </>
-                  )
-                })
+                          </>
+                        )
+                      })
+                  }
+                </>
               }
             </div>
           </div>
--- src/views/Transfer/Home/index.js
+++ src/views/Transfer/Home/index.js
@@ -1,12 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 // import file css
 import './Transfer.css'
 
-//import example photo profile
-import photoProfile from '../../../assets/images/1.png'
+
+import http from '../../../helpers/Http'
+import { useSelector } from 'react-redux'
 
 function Transfer() {
+  const [listUser, setListUser] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
+  const [link, setLink] = useState(null)
+  const user = useSelector(state => state.auth)
+
+  const getListUser = async (token) => {
+    try {
+      const listAllUser = await http(token).get('user')
+      setListUser(listAllUser.data.results) 
+      setLink(listAllUser.data.pageInfo)
+      setIsLoading(!isLoading)
+    } catch (err) {
+      console.log(err)
+      setIsLoading(!isLoading)
+    }
+  }
+
+  const handleClickList = async (event) => {
+    if (event.target.name === 'next') {
+      try {
+        const listAllUser = await http(user.token).get(link.nextLink)
+        setListUser(listAllUser.data.results)
+        setLink(listAllUser.data.pageInfo)
+        setIsLoading(!isLoading)
+      } catch (err) {
+        console.log(err)
+        setIsLoading(!isLoading)
+      }
+    } else if (event.target.name === 'prev') {
+      try {
+        const listAllUser = await http(user.token).get(link.prevLink)
+        setListUser(listAllUser.data.results)
+        setLink(listAllUser.data.pageInfo)
+        setIsLoading(!isLoading)
+      } catch (err) {
+        console.log(err)
+        setIsLoading(!isLoading)
+      }
+    }
+  }
+
+  useEffect(() => {
+    getListUser(user.token)
+  }, [])
+  console.log(link);
+
   return (
     <>
       <div className="card px-4 pt-3 border-rad shadow-sm">
@@ -24,52 +71,75 @@ function Transfer() {
           </div>
         </div>
         <div className="card-body overflow-auto card-list-user">
-          {
-            [1, 2, 3, 4].map(user => {
-              return (
-                <>
-
-                  <div className="card shadow-sm my-2" id={user}>
-                    <div className="card-body">
-                      <div className="row">
-                        <div className="col-1">
-                          <img src={photoProfile} alt="avatar" className="img-profile" />
-                        </div>
-                        <div className="col-11 ps-3">
-                          <span className="fw-bold">
-                            Samuel Suhi
-                            </span>
-                          <br />
-                          <small className="text-muted">
-                            +62 812-4343-6731
-                            </small>
-                        </div>
-                      </div>
+          {listUser === null ?
+            <>
+              {
+                isLoading === true ?
+                  <div className="d-flex justify-content-center align-items-center width-100">
+                    <div className="spinner-border" role="status">
+                      <span className="visually-hidden">Loading...</span>
                     </div>
                   </div>
-                </>
-              )
-            })
+                  :
+                  'User Not Found'
+              }
+            </>
+            :
+            <>
+              {
+                isLoading === true ?
+                  <div className="d-flex justify-content-center align-items-center width-100">
+                    <div className="spinner-border" role="status">
+                      <span className="visually-hidden">Loading...</span>
+                    </div>
+                  </div>
+                  :
+                  listUser.map(user => {
+                    return (
+                      <>
+                        <div className="card shadow-sm my-2" id={user}>
+                          <div className="card-body">
+                            <div className="row">
+                              <div className="col-1">
+                                <img src={user.picture} alt="avatar" className="img-profile" />
+                              </div>
+                              <div className="col-11 ps-3">
+                                <span className="fw-bold">
+                                  {user.first_name === null ? `${user.username}` : `${user.first_name} ${user.last_name}`}
+                                </span>
+                                <br />
+                                <small className="text-muted">
+                                  {user.phone === null ? 'Phone has not been updated' : ''}
+                                </small>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </>
+                    )
+                  })
+              }
+            </>
           }
         </div>
         <div className="card-footer border-0 bg-transparent d-flex justify-content-center align-items-center mt-3 p-0">
-        <nav aria-label="Page navigation example">
-          <ul className="pagination justify-content-center">
-            <li className="page-item">
-              <button className="page-link btn bg-transparent" href="#" tabIndex="-1">
-                <i className="far fa-arrow-left me-3"></i>
-                Prev
-              </button>
-            </li>
-            <li className="page-item"><span className="page-link" href="#">2</span></li>
-            <li className="page-item">
-              <button className="page-link btn bg-transparent" href="#" tabIndex="-1">
-                Next
-                <i className="far fa-arrow-right ms-3"></i>
-              </button>
-            </li>
-          </ul>
-        </nav>
+          <nav aria-label="Page navigation example">
+            <ul className="pagination justify-content-center">
+              <li className="page-item">
+                <button className={`page-link btn bg-transparent ${isLoading === true ? '' : `${link.prevLink === null ? 'disabled' : ''}`}`} href="#" tabIndex="-1" name="prev" onClick={event => handleClickList(event)}>
+                  <i className="far fa-arrow-left me-3"></i>
+                  Prev
+                </button>
+              </li>
+              <li className="page-item"><span className="page-link" href="#">{`${isLoading === true ? '' : `${link.currentPage}`}`}</span></li>
+              <li className="page-item">
+                <button className={`page-link btn bg-transparent ${isLoading === true ? '' : `${link.nextLink === null ? 'disabled' : ''}`}`} href="#" tabIndex="-1" name="next" onClick={event => handleClickList(event)}>
+                  Next
+                  <i className="far fa-arrow-right ms-3"></i>
+                </button>
+              </li>
+            </ul>
+          </nav>
         </div>
       </div>
     </>
